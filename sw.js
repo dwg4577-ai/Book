72704a2db4aea5b38c4b780406dbb35158ac8d9d
dwg4577-ai <dwg4577@gmail.com>
2026-08-21 22:06:08 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "book-stack-v2-4";
+const CACHE = "book-stack-v2-6";
 const ASSETS = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", event => {
